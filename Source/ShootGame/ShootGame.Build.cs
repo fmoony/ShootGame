30bc584c2412898f47f0796d1335ef6a183fa411
FMoony <1161864793@qq.com>
2026-08-17 17:21:56 +0800

    GAS：接入 Gameplay Ability System 模块

--- Source/ShootGame/ShootGame.Build.cs
+++ Source/ShootGame/ShootGame.Build.cs
@@ -18,7 +18,10 @@ public class ShootGame : ModuleRules
 			"StateTreeModule",
 			"GameplayStateTreeModule",
 			"UMG",
-			"Slate"
+			"Slate",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Niagara" });
